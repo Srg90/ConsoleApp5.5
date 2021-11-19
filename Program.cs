b88d368d7d4e329570f186c0234f214cda296482
Srg90 <usikov90@mail.ru>
2021-11-19 20:43:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое число");
+            Console.WriteLine();
+            int N1 = Convert.ToInt32(Console.ReadLine());
+            int N2 = N1;
+            Console.WriteLine();
+            int[,] number = new int[N1, N2];
+            Random random = new Random();
+          
+            for (int i = 0; i < N1; i++)
+            {
+                
+                for (int j = 0; j < N2; j++)
+                {
+                    number[i, j] = random.Next(0, 100);
+                    Console.Write("{0,3} ", number[i, j]);
+                   
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
